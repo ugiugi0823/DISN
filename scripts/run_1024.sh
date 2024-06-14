11bbb8756da52a5ee39fcb869572cf439b3a5eb0
ugiugi0823 <acerghjk@gmail.com>
2024-06-14 11:53:44 +0900

    v2

---
+++ scripts/run_1024.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python run.py \
+--image_path "./img/[0001]TopBF0.png" \
+--prompt "photo of a crack defect image" \
+--neg_prompt " " \
+--bigger
---
+++ scripts/run_dataset_1024.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0 python run_dataset.py \
+--original_dataset_path "./original_dataset" \
+--new_dataset_path "./new_dataset" \
+--prompt "photo of a crack defect image" \
+--neg_prompt " " \
+--bigger \
+--datacheck
