9c5e3387fdcf3d8c3b7554a528f502a20a0583e9
ugiugi0823 <acerghjk@gmail.com>
2024-06-14 17:44:58 +0900

    v2

--- scripts/run_various_test.sh
+++ scripts/run_various_test.sh
@@ -1,9 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python run_various.py \
---image_path "./img/[0001]TopBF0.png" \
---prompt "photo of a crack defect image" \
---ch_prompt "photo of a crack corrosion image" \
---neg_prompt " " \
---bigger
 
 
 
@@ -15,29 +9,6 @@ CUDA_VISIBLE_DEVICES=0 python run_various.py \
 --bigger
 
 
-CUDA_VISIBLE_DEVICES=0 python run_various.py \
---image_path "./img/[0001]TopBF0.png" \
---prompt "photo of a crack defect image" \
---ch_prompt "photo of a crack dent image" \
---neg_prompt " " \
---bigger
-
-
-
-CUDA_VISIBLE_DEVICES=0 python run_various.py \
---image_path "./img/[0001]TopBF0.png" \
---prompt "photo of a crack defect image" \
---ch_prompt "photo of a crack bilstering image" \
---neg_prompt " " \
---bigger
-
-
-CUDA_VISIBLE_DEVICES=0 python run_various.py \
---image_path "./img/[0001]TopBF0.png" \
---prompt "photo of a crack defect image" \
---ch_prompt "photo of a crack rust image" \
---neg_prompt " " \
---bigger
 
 CUDA_VISIBLE_DEVICES=0 python run_various.py \
 --image_path "./img/[0001]TopBF0.png" \
@@ -46,12 +17,6 @@ CUDA_VISIBLE_DEVICES=0 python run_various.py \
 --neg_prompt " " \
 --bigger
 
-CUDA_VISIBLE_DEVICES=0 python run_various.py \
---image_path "./img/[0001]TopBF0.png" \
---prompt "photo of a crack defect image" \
---ch_prompt "photo of a crack degradation image" \
---neg_prompt " " \
---bigger
 
 
 
